2dd7777d46234f6ea7c372608c00566f7104edcc
c4rlysss <carlotta.pasqualon@iisviolamarchesini.edu.it>
2025-01-16 19:52:59 +0100

    aggiunto esercizio 26 con consegna riguardante le classi pag A122 libro 4'

---
+++ src/es26classi.java
@@ -0,0 +1,19 @@
+//I prodotti di un supermercato sono identificati da un codice numerico e per ciascuno di essi si hanno le seguenti
+// infromazioni: codice identificativo, descrizione sintetica, prezzo unitario, quantità presente in negozio, quantità
+// presente in magazzino.
+
+public class es26classi {
+
+       private int codice, quantitaNeg, quantitaMag;
+       private double prezzo;
+       private String desc;
+
+        public es26classi( int codice, int quantitaNeg, int quantitaMag, double prezzo, String desc) {
+            this.codice = codice;
+            this.quantitaNeg = quantitaNeg;
+            this.quantitaMag = quantitaMag;
+            this.prezzo = prezzo;
+            this.desc = desc;
+        }
+
+}
